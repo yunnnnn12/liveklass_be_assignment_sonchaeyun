dd2e5cda1bfc3a034f75b0ddc60b9990c6e149f8
chaeyun_ <sonchaeyun@sonchaeyun-ui-MacBookAir.local>
2026-04-03 16:26:33 +0900

    feat(entity): Add Course entity with lecture details

--- src/main/java/com/liveklass/assignment/data/entity/Course.java
+++ src/main/java/com/liveklass/assignment/data/entity/Course.java
@@ -1,2 +1,36 @@
-package com.liveklass.assignment.data.entity;public class Course {
+package com.liveklass.assignment.data.entity;
+
+import com.liveklass.assignment.data.ClassStatus;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private int maxCapacity;
+
+    // private int currentCount;
+
+    private Long price;
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
+
+    @Enumerated(EnumType.STRING)
+    private ClassStatus classStatus;
+
+
 }
