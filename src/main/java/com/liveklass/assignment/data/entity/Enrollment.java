ed325b9ca4ad55883d1fff478254b49cfc7d8986
chaeyun_ <sonchaeyun@sonchaeyun-ui-MacBookAir.local>
2026-04-03 16:27:58 +0900

    feat(entity): Add Enrollment entity to map Classmate and Course

--- src/main/java/com/liveklass/assignment/data/entity/Enrollment.java
+++ src/main/java/com/liveklass/assignment/data/entity/Enrollment.java
@@ -1,2 +1,33 @@
-package com.liveklass.assignment.data.entity;public class Enrollment {
+package com.liveklass.assignment.data.entity;
+
+import com.liveklass.assignment.data.EnrollmentStatus;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+public class Enrollment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 수강생, 강의
+    // 1. 수강생
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "classmate_id")
+    private Classmate classmate;
+
+    // 2. 강의
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @Enumerated(EnumType.STRING)
+    private EnrollmentStatus enrollmentStatus;
+
+    private LocalDateTime enrolledDate;
+
 }
