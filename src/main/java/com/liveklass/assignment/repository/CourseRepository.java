4b06107ba5685187de2eab8b21bcf8f3d2d5a2cd
chaeyun_ <sonchaeyun@sonchaeyun-ui-MacBookAir.local>
2026-04-04 12:55:59 +0900

    feat(repository): Implement CourseRepository using Spring Data JPA

---
+++ src/main/java/com/liveklass/assignment/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.liveklass.assignment.repository;
+
+import com.liveklass.assignment.data.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
