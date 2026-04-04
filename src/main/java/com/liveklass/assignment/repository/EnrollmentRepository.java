6ab1c763c6bb17cf53eb7db58cda7c09c9fa4a65
chaeyun_ <sonchaeyun@sonchaeyun-ui-MacBookAir.local>
2026-04-04 22:50:23 +0900

    refactor(repository): Add findAllByUserId query method to EnrollmentRepository

--- src/main/java/com/liveklass/assignment/repository/EnrollmentRepository.java
+++ src/main/java/com/liveklass/assignment/repository/EnrollmentRepository.java
@@ -1,2 +1,12 @@
-package com.liveklass.assignment.repository;public class EnrollmentRepository {
+package com.liveklass.assignment.repository;
+
+import com.liveklass.assignment.data.entity.Enrollment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+    List<Enrollment> findAllByUserId(Long userId);
 }
