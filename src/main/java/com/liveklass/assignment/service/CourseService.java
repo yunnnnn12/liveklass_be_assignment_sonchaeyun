68e66ba3a68b15c2c35301554b7aec9ef0e9a059
chaeyun_ <sonchaeyun@sonchaeyun-ui-MacBookAir.local>
2026-04-04 22:56:21 +0900

    feat(service):  Implement course creation, list retrieval, and detail lookup

--- src/main/java/com/liveklass/assignment/service/CourseService.java
+++ src/main/java/com/liveklass/assignment/service/CourseService.java
@@ -1,2 +1,60 @@
-package com.liveklass.assignment.service;public class CourseService {
+package com.liveklass.assignment.service;
+
+import com.liveklass.assignment.data.CourseStatus;
+import com.liveklass.assignment.data.entity.Course;
+import com.liveklass.assignment.dto.CourseCreateRequest;
+import com.liveklass.assignment.dto.CourseResponse;
+import com.liveklass.assignment.repository.CourseRepository;
+import org.springframework.transaction.annotation.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CourseService {
+
+    private final CourseRepository courseRepository;
+
+    // 강의 개설
+    @Transactional
+    public Long createCourse(CourseCreateRequest dto){
+        Course course = Course.builder()
+                .title(dto.title())
+                .description(dto.description())
+                .price(dto.price())
+                .maxCapacity(dto.maxCapacity())
+                .startDate(dto.startDate())
+                .endDate(dto.endDate())
+                .classStatus(CourseStatus.DRAFT)
+                .currentCount(0)
+                .build();
+
+        return courseRepository.save(course).getId();
+    }
+
+    // 강의 목록 조회 (여러개) - 상태값이 null이 아닌 강의 목록
+    public List<CourseResponse> getCourses(CourseStatus status) {
+        List<Course> courses;
+
+        if (status == null) {
+            courses = courseRepository.findAll();
+        } else {
+            courses = courseRepository.findAllByClassStatus(status); // 상태 필터
+        }
+
+        return courses.stream()
+                .map(CourseResponse::from) // Entity를 DTO로 변환
+                .toList();
+    }
+
+    // 강의 상세 조회
+    public CourseResponse getCourseDetail(Long courseId) {
+        Course course = courseRepository.findById(courseId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 강의를 찾을 수 없습니다. ID: " + courseId));
+
+        return CourseResponse.from(course);
+    }
 }
