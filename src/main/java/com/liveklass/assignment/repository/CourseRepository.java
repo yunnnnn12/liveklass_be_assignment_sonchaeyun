d882dc512c1254f63f5e5c9a3e2c27c4aad32b01
chaeyun_ <sonchaeyun@sonchaeyun-ui-MacBookAir.local>
2026-04-04 22:49:00 +0900

    refactor(repository): Add findAllByClassStatus query method to CourseRepository

--- src/main/java/com/liveklass/assignment/repository/CourseRepository.java
+++ src/main/java/com/liveklass/assignment/repository/CourseRepository.java
@@ -1,9 +1,13 @@
 package com.liveklass.assignment.repository;
 
+import com.liveklass.assignment.data.CourseStatus;
 import com.liveklass.assignment.data.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    List<Course> findAllByClassStatus(CourseStatus status); // CourseStatus 기준으로 강의목록 찾기
 }
