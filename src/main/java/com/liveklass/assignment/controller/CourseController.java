fbb6a6b691accd7e97058f404044161508223261
chaeyun_ <sonchaeyun@sonchaeyun-ui-MacBookAir.local>
2026-04-05 18:15:32 +0900

    feat(controller): Add endpoint for creating new courses

---
+++ src/main/java/com/liveklass/assignment/controller/CourseController.java
@@ -0,0 +1,37 @@
+package com.liveklass.assignment.controller;
+
+import com.liveklass.assignment.data.CourseStatus;
+import com.liveklass.assignment.dto.CourseCreateRequest;
+import com.liveklass.assignment.dto.CourseResponse;
+import com.liveklass.assignment.service.CourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
+@RequiredArgsConstructor
+public class CourseController {
+
+    private final CourseService courseService;
+
+    // 1. 강의 개설
+    @PostMapping
+    public ResponseEntity<Long> createCourse(@RequestBody CourseCreateRequest request) {
+        return ResponseEntity.ok(courseService.createCourse(request));
+    }
+
+    // 2. 강의 목록 조회 (상태값 필터링 포함)
+    @GetMapping
+    public ResponseEntity<List<CourseResponse>> getCourses(@RequestParam(name = "status", required = false) CourseStatus status) {
+        return ResponseEntity.ok(courseService.getCourses(status));
+    }
+
+    // 3. 강의 상세 조회
+    @GetMapping("/{courseId}")
+    public ResponseEntity<CourseResponse> getCourseDetail(@PathVariable(name = "courseId") Long courseId) {
+        return ResponseEntity.ok(courseService.getCourseDetail(courseId));
+    }
+}
