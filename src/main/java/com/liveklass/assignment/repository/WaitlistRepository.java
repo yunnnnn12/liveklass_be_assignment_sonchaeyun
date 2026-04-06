b402cca597318e352df1a61ed2fb12562c72f55f
chaeyun_ <sonchaeyun@sonchaeyun-ui-MacBookAir.local>
2026-04-06 22:50:43 +0900

    feat(repository): Add WaitlistRepository for queue management

---
+++ src/main/java/com/liveklass/assignment/repository/WaitlistRepository.java
@@ -0,0 +1,9 @@
+package com.liveklass.assignment.repository;
+
+import com.liveklass.assignment.data.entity.Waitlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WaitlistRepository extends JpaRepository<Waitlist, Long> {
+}
