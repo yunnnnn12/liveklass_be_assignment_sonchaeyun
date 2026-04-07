06c55a92356a76c4b0b0345c4bf7ea4c257be24a
chaeyun_ <sonchaeyun@sonchaeyun-ui-MacBookAir.local>
2026-04-07 15:41:29 +0900

    feat(repository): Add findByName to ClassmateRepository and refactor package structure

--- src/main/java/com/liveklass/assignment/data/repository/ClassmateRepository.java
+++ src/main/java/com/liveklass/assignment/data/repository/ClassmateRepository.java
@@ -1,7 +1,10 @@
-package com.liveklass.assignment.repository;
+package com.liveklass.assignment.data.repository;
 
 import com.liveklass.assignment.data.entity.Classmate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClassmateRepository extends JpaRepository<Classmate, Long> {
+    Optional<Classmate> findByName(String name);
 }
